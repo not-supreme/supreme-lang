b2e5dba471418d596fc1507873d7c372d81cbbea
adrian <35572104+zwloaddriver@users.noreply.github.com>
2020-05-17 18:58:45 +0200

    improved ABI

--- SupremeLang/SupremeVM/inc/svm.h
+++ SupremeLang/SupremeVM/inc/svm.h
@@ -9,17 +9,18 @@ typedef struct _svm_cpu_t
 
 typedef struct _svm_section_t
 {
+	uint16_t sec_flags;
 	uintptr_t sec_rva;
 	uintptr_t sec_size;
 } svm_section_t;
 
 typedef struct _svm_executable_t
 {
-	uint32_t magic; // should be 'EmvS' (SvmE), used to validate file
-	uint32_t version; // compiler version
-	uint32_t extra_data[ 16 ]; // for future ABI compatibility
-	svm_section_t section_code; // (RX) readable executable memory
-	svm_section_t section_data; // (RW) readable writeable memory
+	uint32_t magic;             // should be 'EmvS' (SvmE), used to validate file
+	uint32_t version;           // compiler version
+	uintptr_t entry_rva;        // rva to entrypoint
+	svm_section_t section_code; // code, executable
+	svm_section_t section_data; // data, non-executable
 } svm_executable_t;
 
 #pragma pack(pop)
